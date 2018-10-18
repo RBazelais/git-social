bcd650382973d3fde362bc7a109f2b9c017ceaf3
Rachél Bazelais <bazelais.rachel@gmail.com>
2018-10-18 01:34:43 -0700

    Added the ability to create posts and validate their text

--- models/Post.js
+++ models/Post.js
@@ -1,62 +1,56 @@
-// Post Model
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 // Create Schema
 const PostSchema = new Schema({
-	// Associate user by ID
+user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+},
+text: {
+    type: String,
+    required: true
+},
+name: {
+    type: String
+},
+avatar: {
+    type: String
+},
+likes: [
+    {
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+    }
+    }
+],
+comments: [
+    {
     user: {
         type: Schema.Types.ObjectId,
         ref: 'users'
     },
-	text: {
-		type: String,
-		required: true
-	},
-	name: {
-        type: String 
+    text: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String
     },
     avatar: {
         type: String
     },
-    // Likes are an array of user_id(s)
-    likes: [
-        {
-            user: {
-                type: Schema.Types.ObjectId,
-                ref: 'users'
-            }
-            
-        }
-    ],
-    // comments include the user's name, avatar and date
-    comments: [
-        {
-            user: {
-                type: Schema.Types.ObjectId,
-                ref: 'users'
-            },
-            text: {
-                type: String,
-                required: true
-            },
-            name: {
-                type: String 
-            },
-            avatar: {
-                type: String
-            },
-            date: {
-                type: Date,
-                Default: Date.Now
-            }
-        }
-    ],
-    // The post itself has a date
     date: {
         type: Date,
-        Default: Date.Now
+        default: Date.now
     }
+    }
+],
+date: {
+    type: Date,
+    default: Date.now
+}
 });
 
 module.exports = Post = mongoose.model('post', PostSchema);
